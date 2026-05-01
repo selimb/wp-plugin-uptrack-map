f4b4c4d07128fa3302ba7bdb5c1505d994f9c438
Selim Belhaouane <selim.belhaouane@gmail.com>
2026-05-01 16:32:42 -0700

    fix

--- src/settings.test.ts
+++ src/settings.test.ts
@@ -7,9 +7,9 @@ test("zUptrackSettingsSafe", () => {
   const result = zUptrackSettingsSafe.parse("oopsies");
   expect(result).toMatchInlineSnapshot(`
     {
-      "image_distance": "",
-      "image_duration": "",
-      "image_elevation": "",
+      "alpinejs_url": "https://cdn.jsdelivr.net/npm/alpinejs@3.15.11/dist/cdn.min.js",
+      "css": "/home/selimb/dev/personal/wp-plugin-uptrack-map/src/default-assets/uptrack-map.css",
+      "focus_card_html": HTMLBundle {},
       "kml_directory": "kml-paths",
       "routes": [],
     }
