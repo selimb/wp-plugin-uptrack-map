1d88b53f5760316338fffd33550e251427a4afe8
Selim Belhaouane <selim.belhaouane@gmail.com>
2026-05-03 17:30:37 -0700

    fix php

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,5 +1,3 @@
-import * as z from "zod/mini";
-
 export function clamp(v: number, min: number, max: number): number {
   // eslint-disable-next-line unicorn/prefer-math-min-max -- This is simpler.
   return v < min ? min : v > max ? max : v;
